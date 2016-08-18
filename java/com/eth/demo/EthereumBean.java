4902b4cf1850997d28f2210e16776a1d981b7a55
mbhooshan <mani.bhooshan@gmail.com>
2016-08-17 23:36:41 -0700

    fixed issue

---
+++ java/com/eth/demo/EthereumBean.java
@@ -0,0 +1,25 @@
+package com.eth.demo;
+
+import org.ethereum.facade.Ethereum;
+import org.ethereum.facade.EthereumFactory;
+
+
+public class EthereumBean {
+
+    Ethereum ethereum;
+
+    public void start(){
+        this.ethereum = EthereumFactory.createEthereum();
+        this.ethereum.addListener(new EthereumDemo(ethereum));
+    }
+
+
+    public String getBestBlock(){
+        return "" + ethereum.getBlockchain().getBestBlock().getNumber();
+    }
+
+    public static void main(String[] args) {
+    	EthereumBean ethBean = new EthereumBean();
+    	ethBean.start();
+	}
+}
